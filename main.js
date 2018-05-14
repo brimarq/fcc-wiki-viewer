2f6d1847717f0dec0b8157768d6306ee3e42d079
Brian Marquis <rbmarquis.dev@gmail.com>
2018-05-13 20:17:55 -0500

    Using .list-group for search results. Provide generic thumbnail if none in query.

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
-  var wikiPages = [];
-  /** Round function from MDN, addresses funky JS math issues.
-   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#A_better_solution
-   */
-  function round(number, precision) {
-    var shift = function (number, precision) {
-      var numArray = ("" + number).split("e");
-      return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
-    };
-    return shift(Math.round(shift(number, +precision)), -precision);
-  }
+  //var wikiPages = [];
 
   $('#wikiSearch').submit(function(e){
     
@@ -18,7 +8,7 @@ $(document).ready(function() {
     console.log(searchTerm);
 
     $.ajax({
-      url: '//en.wikipedia.org/w/api.php',
+      url: 'https://en.wikipedia.org/w/api.php',
       data: {
         action: 'query',
         generator: 'search',
@@ -27,7 +17,7 @@ $(document).ready(function() {
         prop: 'info|pageimages|extracts', 
         inprop: 'url',
         piprop: 'thumbnail|name',
-        pithumbsize: 50,
+        pithumbsize: 100,
         pilicense: 'any',
         exintro: true,
         explaintext: true,
@@ -38,19 +28,30 @@ $(document).ready(function() {
       },
       dataType: 'jsonp',
       success: function (x) {
-        var arr = x.query.pages;
-        console.log(x.query.pages);
-        // $.wikiPages = x.query.pages;
-        console.log(arr[0].title);
-        console.log(arr[0].extract);
-        console.log(arr[0].canonicalurl);
-        console.log(arr[0].thumbnail.source);
-        var i;
-        $('#search-results').empty();
-        for (i = 0; i < arr.length; i++) {
-          $('#search-results').append(
-            '<div class="media"><img class="media-thumbnail mr-3" src="' + arr[i].thumbnail.source + '" alt="thumbnail img"><div class="media-body"><h5 class="media-title mt-0">' + arr[i].title + '</h5><p class="media-extract">' + arr[i].extract + '</p></div><!-- .media-body --></div><!-- .media -->'
-          );
+        // What to do with a bad query:
+        if (!x.query) {
+          return console.log("OOPS! BAD QUERY!");
+        } 
+        // Do this when query is returned
+        else {
+          // arr variable scoped to this block
+          let arr = x.query.pages;
+          console.log(arr[0].title);
+          console.log(arr[0].extract);
+          console.log(arr[0].canonicalurl);
+          //console.log(arr[0].thumbnail.source);
+          var i;
+          $('#search-results').empty();
+          for (i = 0; i < arr.length; i++) {
+            // Provide a generic thumbnail for the item if there isn't one.
+            if (!arr[i].thumbnail) {
+              arr[i].thumbnail = {source: "https://en.wikipedia.org/static/images/project-logos/enwiki.png"};
+            }
+            // Write the results to the page
+            $('#search-results').append(
+              '<a href="' + arr[i].canonicalurl + '" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start"><div class="media"><img class="thumbnail mr-3" src="' + arr[i].thumbnail.source + '" alt="Thumbnail image"><div class="media-body"><h5 class="mb-1">' + arr[i].title + '</h5><p class="mb-0">' + arr[i].extract + '</p></div><!-- .media-body --></div><!-- .media --></a><!-- .list-group-item -->'
+            );
+          }
         }
       }
     });
@@ -62,9 +63,12 @@ $(document).ready(function() {
 
 
 
-  // Toggle temperature between F and C.
-  $('.tempToggle').on('click', function() {
-    $('.temp').toggle();
+  $('.list-group a').on('mouseenter', function() {
+    $(this).addClass('active');
+  });
+
+  $('.list-group a').on('mouseleave', function() {
+    $(this).removeClass('active');
   });
 
 });
